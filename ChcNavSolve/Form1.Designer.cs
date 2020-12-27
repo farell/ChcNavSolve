ef356b4b33a22f3ea13a8c158f475abee3b2c691
lei wang <wangleizszxxx@163.com>
2020-12-27 10:52:08 +0800

    Initial release

---
+++ ChcNavSolve/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace ChcNavSolve
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series5 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series6 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series7 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxPort = new System.Windows.Forms.TextBox();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.buttonStop = new System.Windows.Forms.Button();
+            this.textBoxLog = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(12, 85);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series2.Legend = "Legend1";
+            series2.Name = "Series2";
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series3.Legend = "Legend1";
+            series3.Name = "Series3";
+            series4.ChartArea = "ChartArea1";
+            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series4.Legend = "Legend1";
+            series4.Name = "Series4";
+            series5.ChartArea = "ChartArea1";
+            series5.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series5.Legend = "Legend1";
+            series5.Name = "Series5";
+            series6.ChartArea = "ChartArea1";
+            series6.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series6.Legend = "Legend1";
+            series6.Name = "Series6";
+            series7.ChartArea = "ChartArea1";
+            series7.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series7.Legend = "Legend1";
+            series7.Name = "Series7";
+            this.chart1.Series.Add(series1);
+            this.chart1.Series.Add(series2);
+            this.chart1.Series.Add(series3);
+            this.chart1.Series.Add(series4);
+            this.chart1.Series.Add(series5);
+            this.chart1.Series.Add(series6);
+            this.chart1.Series.Add(series7);
+            this.chart1.Size = new System.Drawing.Size(776, 353);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.buttonStop);
+            this.groupBox1.Controls.Add(this.buttonStart);
+            this.groupBox1.Controls.Add(this.textBoxPort);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(776, 66);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "控制";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "端口:";
+            // 
+            // textBoxPort
+            // 
+            this.textBoxPort.Location = new System.Drawing.Point(48, 18);
+            this.textBoxPort.Name = "textBoxPort";
+            this.textBoxPort.Size = new System.Drawing.Size(63, 21);
+            this.textBoxPort.TabIndex = 1;
+            this.textBoxPort.Text = "6789";
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(469, 21);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(75, 23);
+            this.buttonStart.TabIndex = 2;
+            this.buttonStart.Text = "Start";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // buttonStop
+            // 
+            this.buttonStop.Location = new System.Drawing.Point(619, 21);
+            this.buttonStop.Name = "buttonStop";
+            this.buttonStop.Size = new System.Drawing.Size(75, 23);
+            this.buttonStop.TabIndex = 3;
+            this.buttonStop.Text = "Stop";
+            this.buttonStop.UseVisualStyleBackColor = true;
+            this.buttonStop.Click += new System.EventHandler(this.buttonStop_Click);
+            // 
+            // textBoxLog
+            // 
+            this.textBoxLog.Location = new System.Drawing.Point(12, 445);
+            this.textBoxLog.Multiline = true;
+            this.textBoxLog.Name = "textBoxLog";
+            this.textBoxLog.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxLog.Size = new System.Drawing.Size(776, 147);
+            this.textBoxLog.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(808, 604);
+            this.Controls.Add(this.textBoxLog);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBoxPort;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonStop;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.TextBox textBoxLog;
+    }
+}
+
---
+++ ChcNavSolve/Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ChcNavSolve
+{
+    public partial class Form1 : Form
+    {
+        private TcpClient tcpClient;
+        private BackgroundWorker backgroundWorker;
+        private System.Timers.Timer hourTimer;
+        private System.Timers.Timer minuteTimer;
+
+        private double[] init = { 0, 0, 2912677.7541,2912760.4699,2912879.327,2912990.7365,2913089.3157,2912677.7541 };
+        public Form1()
+        {
+            InitializeComponent();
+            //tcpClient = new TcpClient("localhost", Int32.Parse(textBoxPort.Text));
+            backgroundWorker = new BackgroundWorker();
+            backgroundWorker.WorkerSupportsCancellation = true;
+            backgroundWorker.DoWork += BackgroundWorker_DoWork;
+
+            hourTimer = new System.Timers.Timer(1000 * 20);
+            hourTimer.Elapsed += new System.Timers.ElapsedEventHandler(HourTimer_TimesUp);
+            hourTimer.AutoReset = true; //每到指定时间Elapsed事件是触发一次（false），还是一直触发（true）
+            minuteTimer = new System.Timers.Timer(1000 * 3);
+            minuteTimer.Elapsed += new System.Timers.ElapsedEventHandler(MinuteTimer_TimesUp);
+            minuteTimer.AutoReset = false; //每到指定时间Elapsed事件是触发一次（false），还是一直触发（true）
+        }
+
+        private void HourTimer_TimesUp(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            //isSaveRawData = true;
+            minuteTimer.Start();
+
+            AppendLog(DateTime.Now.ToString()+" HourTimer...up");
+        }
+
+        private void MinuteTimer_TimesUp(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            //isSaveRawData = false;
+            AppendLog(DateTime.Now.ToString() + " MinuteTimer...up");
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            //backgroundWorker.RunWorkerAsync();
+            hourTimer.Start();
+        }
+
+        private void BackgroundWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            BackgroundWorker bgWorker = sender as BackgroundWorker;
+            //string stamp = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+            byte[] data = new Byte[1024];
+            NetworkStream stream = tcpClient.GetStream();
+
+            while (true)
+            {
+                try
+                {
+                    if (tcpClient.Connected)
+                    {
+                        // Read the first batch of the TcpServer response bytes.
+                        Int32 bytes = stream.Read(data, 0, data.Length);
+                        int offset = 51;
+                        int n = bytes / 51;
+
+                        if (n < 1)
+                        {
+                            continue;
+                        }
+
+                        for(int i = 0; i < n; i++)
+                        {
+                            if(data[0+offset*i] == 0x24 && data[49+offset*i]==0x0d && data[50 + offset * i] == 0x0a)
+                            {
+                                int stationId = data[12 + offset * i];
+                                double x = BitConverter.ToDouble(data, 24 + i * offset);
+                                double y = BitConverter.ToDouble(data, 32 + i * offset);
+                                double z = BitConverter.ToDouble(data, 40 + i * offset);
+
+                                string content = "station id: " + stationId + " x: " + x.ToString() + " y: " + y.ToString() + " z: " + z.ToString();
+
+                                AppendLog(content);
+                                UpdateChart(stationId, (x-init[stationId])*1000);
+                                //chart1.Series[0].Points.AddY(x);
+                                //chart1.Series[0].Points.AddY(x);
+                                //chart1.Series[0].Points.AddY(x);
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    if (backgroundWorker.CancellationPending == true)
+                    {
+                        e.Cancel = true;
+                        break;
+                    }
+                    stream.Close();
+                    using (StreamWriter sw = new StreamWriter(@"ErrLog.txt", true))
+                    {
+                        sw.WriteLine(ex.Message + " \r\n" + ex.StackTrace.ToString());
+                        sw.WriteLine("---------------------------------------------------------");
+                        sw.Close();
+                    }
+                }
+            }
+        }
+
+        private void UpdateChart(int station,double x)
+        {
+            if (chart1.InvokeRequired)
+            {
+                chart1.BeginInvoke(new MethodInvoker(() =>
+                {
+                    chart1.Series[station].Points.AddY(x);
+
+                    if (chart1.Series[station].Points.Count > 256)
+                    {
+                        chart1.Series[station].Points.RemoveAt(0);
+                    }
+                    chart1.ResetAutoValues();
+                    chart1.Invalidate();
+                }));
+            }
+            else
+            {
+                chart1.Series[station].Points.AddY(x);
+
+                if (chart1.Series[station].Points.Count > 256)
+                {
+                    chart1.Series[station].Points.RemoveAt(0);
+                }
+                chart1.ResetAutoValues();
+                chart1.Invalidate();
+            }
+        }
+
+        private void AppendLog(string message)
+        {
+            if (textBoxLog.InvokeRequired)
+            {
+                textBoxLog.BeginInvoke(new MethodInvoker(() =>
+                {
+                    textBoxLog.AppendText(message + " \r\n");
+                }));
+            }
+            else
+            {
+                textBoxLog.AppendText(message + " \r\n");
+            }
+        }
+
+        private void buttonStop_Click(object sender, EventArgs e)
+        {
+            //tcpClient.Close();
+            //backgroundWorker.CancelAsync();
+            hourTimer.Stop();
+        }
+    }
+}
